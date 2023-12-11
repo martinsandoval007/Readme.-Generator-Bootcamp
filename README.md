1ff9a9f188e0388e9ed1ecacbd618be923281699
martin <martinsandoval007@gmail.com>
2023-12-11 00:06:00 -0600

    added questions and write to file and generateMarkdow

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Readme.-Generator-Bootcamp
-creating a Readme file generator so it can help individuals make Readme
+# undefined
+  
+
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require(`inquirer`);
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
@@ -19,17 +20,72 @@ const questions = () => {
             }
         },
         {
-            
-        }
+            type: 'input',
+            name: 'project-name',
+            message: 'Please enter your project-name',
+            validate : Input => {
+                if(Input){
+                    return true;
+                }else{
+                    console.log('Please enter project-name!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Please enter description of your project',
+            validate : Input => {
+                if(Input){
+                    return true;
+                }else{
+                    console.log('Please enter description of your project!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'Please enter how to install project',
+            validate : Input => {
+                if(Input){
+                    return true;
+                }else{
+                    console.log('Please enter how to install project.')
+                    return false;
+                }
+            }
+        },
+        
 
     ]);
 };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err)=>{
+        if (err){
+            console.error(err);
+            return;
+        }
+        console.log('README file created sucessfully')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+  .then((answers) => {
+    // Process the user's answers here
+    const readmeData = generateMarkdown(answers)
+    writeToFile('README.md', readmeData);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+}
 
 // Function call to initialize app
 init();
